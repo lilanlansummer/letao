cf54a736f6d7f008e7a0fc5587f3c6929dc6f0e3
lilanlansummer <13734085066@163.com>
2018-09-29 09:36:41 +0800

    完成登录

--- js/common.js
+++ js/common.js
@@ -1,8 +1,7 @@
+NProgress.start();
 
-	NProgress.start();
+NProgress.done();
 
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+$('.navs ul').prev('a').on('click', function() {
+    $(this).next().slideToggle();
+});
